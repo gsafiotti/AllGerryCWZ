6f335b9756aa4653505adccb2b9a99061322f289
gesaf <safiottigerardo@gmail.com>
2018-04-10 16:00:23 +0200

    creata classe gallina

---
+++ app/src/main/java/gerry/allgerry/Gallina.java
@@ -0,0 +1,32 @@
+package gerry.allgerry;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+public class Gallina {
+    String id;
+    String nome;
+    int posizioneInCampo;
+    boolean inMano = false;
+    boolean inCampo = false;
+    int attacco;
+    int dannoFattoria;
+    int vita;
+    int mana;
+    String areaDanno;
+    String tipo;
+    String abilita;
+
+    Bitmap img_mini;
+    Bitmap img_tipo;
+    Bitmap img_big;
+    Bitmap img_casella;
+
+    public Gallina(String id, String nome, int posizioneInCampo,boolean inMano, boolean inCampo,
+            int attacco, int dannoFattoria, int vita, int mana, String areaDanno, String tipo,
+            String abilita, Bitmap img_mini, Bitmap img_tipo, Bitmap img_big){
+
+
+    }
+}
